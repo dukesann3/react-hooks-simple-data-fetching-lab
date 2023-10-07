dd50781c77db642458a0407e5cc743d2b97c6c17
Duke <dukemorise33@gmail.com>
2023-10-06 22:47:55 -0700

    data fetching with useEffect done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import { useEffect, useState } from "react"
+
+function App() {
+
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const [imageSRC, setImageSRC] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                const { message } = data;
+                setImageSRC(message);
+            });
+    }, []);
+
+    function imageLoad() {
+        if (!imageSRC) {
+            return <p>Loading...</p>
+        }
+
+        return <img src={imageSRC} alt='A Random Dog'></img>
+    }
+
+
+    return (
+        <div>
+            {imageLoad()}
+        </div>
+    )
+}
+
+export default App;
